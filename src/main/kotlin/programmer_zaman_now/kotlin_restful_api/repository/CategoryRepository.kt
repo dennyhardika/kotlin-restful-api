83672b4a23798a2ff780400b510c3ad169e32f0f
dennyhardika <121499206+dennyhardika@users.noreply.github.com>
2025-02-06 20:24:34 +0700

    Update CategoryRepository.kt

--- src/main/kotlin/programmer_zaman_now/kotlin_restful_api/repository/CategoryRepository.kt
+++ src/main/kotlin/programmer_zaman_now/kotlin_restful_api/repository/CategoryRepository.kt
@@ -4,4 +4,5 @@ import org.springframework.data.jpa.repository.JpaRepository
 import programmer_zaman_now.kotlin_restful_api.entity.Category
 
 interface CategoryRepository: JpaRepository<Category, Long> {
+  fun findByIdKategori(id_kategori: Long): Category?
 }
