1bc0d93f3046da1b737c3344cf0e6aa4314f90f6
dennyhardika <121499206+dennyhardika@users.noreply.github.com>
2025-02-07 14:04:24 +0700

    Update CategoryRepository.kt

--- src/main/kotlin/programmer_zaman_now/kotlin_restful_api/repository/CategoryRepository.kt
+++ src/main/kotlin/programmer_zaman_now/kotlin_restful_api/repository/CategoryRepository.kt
@@ -6,5 +6,6 @@ import programmer_zaman_now.kotlin_restful_api.entity.Category
 interface CategoryRepository: JpaRepository<Category, Long> {
   fun findByIdkategori(idKategori: Long): Category?
   
-  fun findByNamakategori(namakategori: String?): Category?
+  @Query("SELECT c FROM Category c WHERE LOWER(c.namakategori) = LOWER(:namakategori)")
+  fun findByNamakategori(@Param("namakategori") namakategori: String): Category?
 }
