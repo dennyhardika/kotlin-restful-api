3dc91561a5fd16a8ce9b2bd2c872967e89d0d128
dennyhardika <121499206+dennyhardika@users.noreply.github.com>
2025-02-13 20:47:48 +0700

    Update UprofileService.kt

--- src/main/kotlin/programmer_zaman_now/kotlin_restful_api/service/UprofileService.kt
+++ src/main/kotlin/programmer_zaman_now/kotlin_restful_api/service/UprofileService.kt
@@ -1,5 +1,6 @@
 package programmer_zaman_now.kotlin_restful_api.service
 
+import org.springframework.web.multipart.MultipartFile
 import programmer_zaman_now.kotlin_restful_api.entity.User
 import programmer_zaman_now.kotlin_restful_api.model.uprofile.CreateUprofileRequest
 import programmer_zaman_now.kotlin_restful_api.model.uprofile.ListUprofileRequest
@@ -8,11 +9,14 @@ import programmer_zaman_now.kotlin_restful_api.model.uprofile.UprofileResponse
 
 interface UprofileService {
 
-    fun create(createUprofileRequest: CreateUprofileRequest, user: User): UprofileResponse
+    fun create(
+        createUprofileRequest: CreateUprofileRequest, fotoProfil: User,
+        fotoKendaraan: MultipartFile?, user: MultipartFile
+    ): UprofileResponse
 
     fun get(id: Long): UprofileResponse
 
-    fun update(id: Long, updateUprofileRequest: UpdateUprofileRequest): UprofileResponse
+    fun update(id: Long, updateUprofileRequest: UpdateUprofileRequest, fotoProfil: MultipartFile?, fotoKendaraan: MultipartFile?): UprofileResponse
 
     fun delete(id: Long)
 
