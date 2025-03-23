e5b57a6958bf924728ff24712f20840f5754ac3a
dennyhardika <dennyhardika70@gmail.com>
2025-03-23 08:19:35 +0700

    first commit

---
+++ src/main/kotlin/programmer_zaman_now/kotlin_restful_api/error/NotFoundException.kt
@@ -0,0 +1,4 @@
+package programmer_zaman_now.kotlin_restful_api.error
+
+class NotFoundException {
+}
---
+++ src/main/kotlin/programmer_zaman_now/kotlin_restful_api/service/FileStorageService.kt
@@ -0,0 +1,4 @@
+package programmer_zaman_now.kotlin_restful_api.service
+
+class FileStorageService {
+}
