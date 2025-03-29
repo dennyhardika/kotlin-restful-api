f188f0248bd9cc3e9062002d43b5f2fcc3482554
dennyhardika <121499206+dennyhardika@users.noreply.github.com>
2025-03-29 20:44:58 +0700

    Update OrderRepository.kt

--- src/main/kotlin/programmer_zaman_now/kotlin_restful_api/repository/OrderRepository.kt
+++ src/main/kotlin/programmer_zaman_now/kotlin_restful_api/repository/OrderRepository.kt
@@ -1,7 +1,11 @@
 package programmer_zaman_now.kotlin_restful_api.repository
 
 import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.data.jpa.repository.Query
+import org.springframework.data.repository.query.Param
 import programmer_zaman_now.kotlin_restful_api.entity.Orders
+import programmer_zaman_now.kotlin_restful_api.entity.orders.Packet
 
 interface OrderRepository : JpaRepository<Orders, Long>{
+    fun findByUprofiles_Iduprofile(iduprofile: Long): List<Orders>
 }
