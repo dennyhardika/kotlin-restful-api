450b52f9cf48d8856021e1f064685016c9cab829
dennyhardika <121499206+dennyhardika@users.noreply.github.com>
2025-04-02 09:33:15 +0700

    Update UpdateCategoryRequest.kt

--- src/main/kotlin/programmer_zaman_now/kotlin_restful_api/model/category/UpdateCategoryRequest.kt
+++ src/main/kotlin/programmer_zaman_now/kotlin_restful_api/model/category/UpdateCategoryRequest.kt
@@ -1,5 +1,6 @@
 package programmer_zaman_now.kotlin_restful_api.model.category
 
+import com.fasterxml.jackson.annotation.JsonProperty
 import jakarta.validation.constraints.NotBlank
 import jakarta.validation.constraints.NotNull
 import programmer_zaman_now.kotlin_restful_api.model.product.UpdateProductRequest
@@ -7,6 +8,10 @@ import programmer_zaman_now.kotlin_restful_api.model.product.UpdateProductReques
 data class UpdateCategoryRequest(
 
     @field:NotBlank
-    val namakategori: String?
+    val namakategori: String?,
+
+    @JsonProperty("group")
+    @field:NotNull
+    val idgrup: Long
 
 )
