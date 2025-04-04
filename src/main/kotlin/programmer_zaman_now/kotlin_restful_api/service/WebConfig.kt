049bd7f54337b86f88054fcab5ae603154bbdba7
dennyhardika <121499206+dennyhardika@users.noreply.github.com>
2025-04-05 00:37:23 +0700

    Add files via upload

---
+++ src/main/kotlin/programmer_zaman_now/kotlin_restful_api/service/WebConfig.kt
@@ -0,0 +1,13 @@
+package programmer_zaman_now.kotlin_restful_api.service
+
+import org.springframework.context.annotation.Configuration
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
+
+@Configuration
+class WebConfig : WebMvcConfigurer {
+    override fun addResourceHandlers(registry: ResourceHandlerRegistry) {
+        registry.addResourceHandler("/uploads/**")
+            .addResourceLocations("file:/home/uploads/")
+    }
+}
