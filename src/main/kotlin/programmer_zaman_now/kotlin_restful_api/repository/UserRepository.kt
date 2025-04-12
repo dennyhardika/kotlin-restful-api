e5bee7add8adfd5e04f9246964bb65c7d70b76dd
dennyhardika <121499206+dennyhardika@users.noreply.github.com>
2025-04-12 18:27:07 +0700

    Update UserRepository.kt

--- src/main/kotlin/programmer_zaman_now/kotlin_restful_api/repository/UserRepository.kt
+++ src/main/kotlin/programmer_zaman_now/kotlin_restful_api/repository/UserRepository.kt
@@ -1,7 +1,14 @@
 package programmer_zaman_now.kotlin_restful_api.repository
 
 import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.data.jpa.repository.Query
+import org.springframework.data.repository.query.Param
+import programmer_zaman_now.kotlin_restful_api.entity.Uprofile
 import programmer_zaman_now.kotlin_restful_api.entity.User
 
 interface UserRepository : JpaRepository<User, Long> {
+
+    @Query("SELECT c FROM User c WHERE LOWER(c.username) = LOWER(:username)")
+    fun findByUsername(@Param("username") username: String): User?
+
 }
