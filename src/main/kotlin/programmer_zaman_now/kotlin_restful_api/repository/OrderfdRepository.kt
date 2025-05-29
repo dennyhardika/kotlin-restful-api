4ba9281ae4a69b3a1ff6abdea4dcc77aff73a2aa
dennyhardika <121499206+dennyhardika@users.noreply.github.com>
2025-05-29 20:48:53 +0700

    Update OrderfdRepository.kt

--- src/main/kotlin/programmer_zaman_now/kotlin_restful_api/repository/OrderfdRepository.kt
+++ src/main/kotlin/programmer_zaman_now/kotlin_restful_api/repository/OrderfdRepository.kt
@@ -1,11 +1,16 @@
 package programmer_zaman_now.kotlin_restful_api.repository
 
+import jakarta.persistence.Id
 import org.springframework.data.jpa.repository.JpaRepository
 import programmer_zaman_now.kotlin_restful_api.entity.Orders
+import programmer_zaman_now.kotlin_restful_api.entity.Product
 import programmer_zaman_now.kotlin_restful_api.entity.orders.Ordersfd
 
 interface OrderfdRepository : JpaRepository<Ordersfd, Long> {
 
-    // fun findByOrders_IdOrder(idOrder: Long): List<Ordersfd>
+//    fun findByOrders_IdOrder(idOrder: Long): List<Ordersfd>
+
+    // Query untuk mencari Orderfd berdasarkan ID Order
+    fun findByOrdersfd_Idorderfd(orderfdId: Long): List<Ordersfd>
 
 }
