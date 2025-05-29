ab43166d12675abca5a91ee71233962d8da424cb
dennyhardika <121499206+dennyhardika@users.noreply.github.com>
2025-05-29 16:57:55 +0700

    Add files via upload

---
+++ src/main/kotlin/programmer_zaman_now/kotlin_restful_api/repository/OrderfdRepository.kt
@@ -0,0 +1,11 @@
+package programmer_zaman_now.kotlin_restful_api.repository
+
+import org.springframework.data.jpa.repository.JpaRepository
+import programmer_zaman_now.kotlin_restful_api.entity.Orders
+import programmer_zaman_now.kotlin_restful_api.entity.orders.Ordersfd
+
+interface OrderfdRepository : JpaRepository<Ordersfd, Long> {
+
+    fun findByOrdersfd_Idorderfd(idorderfd: Long): List<Ordersfd>
+
+}
