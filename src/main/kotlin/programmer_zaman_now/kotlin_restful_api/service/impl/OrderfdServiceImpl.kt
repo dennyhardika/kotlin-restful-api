366666df2f4c731e19571973bbc217ac19a2c484
dennyhardika <121499206+dennyhardika@users.noreply.github.com>
2025-05-29 21:20:15 +0700

    Update OrderfdServiceImpl.kt

--- src/main/kotlin/programmer_zaman_now/kotlin_restful_api/service/impl/OrderfdServiceImpl.kt
+++ src/main/kotlin/programmer_zaman_now/kotlin_restful_api/service/impl/OrderfdServiceImpl.kt
@@ -65,7 +65,7 @@ class OrderfdServiceImpl(val orderfdRepository: OrderfdRepository, val orderRepo
     // Metode baru untuk mendapatkan produk berdasarkan kategori
     override fun getOrderfdByOrder(id_order: Long): List<OrderfdResponse> {
         // Ambil semua produk berdasarkan categoryId
-        val orderfd = orderfdRepository.findByOrders_IdOrder(id_order)
+        val orderfd = orderfdRepository.findByOrders_Id_order(id_order)
 
         // Mengonversi daftar produk ke daftar response untuk API
         return orderfd.map { convertOrderfdToOrderfdResponse(it) }
