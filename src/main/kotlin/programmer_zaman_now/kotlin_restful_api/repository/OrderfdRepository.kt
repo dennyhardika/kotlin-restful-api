f6d1ca8fa2d3a068c9a78d01026252f413ed2fc6
dennyhardika <121499206+dennyhardika@users.noreply.github.com>
2025-05-29 20:58:53 +0700

    Update OrderfdRepository.kt

--- src/main/kotlin/programmer_zaman_now/kotlin_restful_api/repository/OrderfdRepository.kt
+++ src/main/kotlin/programmer_zaman_now/kotlin_restful_api/repository/OrderfdRepository.kt
@@ -11,6 +11,6 @@ interface OrderfdRepository : JpaRepository<Ordersfd, Long> {
 //    fun findByOrders_IdOrder(idOrder: Long): List<Ordersfd>
 
     // Query untuk mencari Orderfd berdasarkan ID Order
-    fun findByOrdersfd_Idorderfd(orderfdId: Long): List<Ordersfd>
+    fun findByOrdersfd_Idorderfd(id_orderfd: Long): List<Ordersfd>
 
 }
